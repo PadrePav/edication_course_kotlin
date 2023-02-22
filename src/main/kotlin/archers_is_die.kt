f6650e3f471fde063b8cd80693520b18008aa117
Padre <kuharukpavel@gmail.com>
2023-02-22 17:29:54 +0200

    Solving this problem with a single array

--- src/main/kotlin/archers_is_die.kt
+++ src/main/kotlin/archers_is_die.kt
@@ -1,63 +1,36 @@
 fun main() {
     print("Enter the number of archers: ")
-    val countArchers = readln().toInt()
     val archersList: MutableList<Int> = mutableListOf()
-    for (i in 1..countArchers) {
-        archersList.add(i)
+    for (i in 1..readln().toInt()) {
+        if (i % 2 == 0)
+            archersList.add(0)                          // 0 stands for dead archer
+        else
+            archersList.add(i)
     }
-    val secondArchList: MutableList<Int> = mutableListOf()
-    var check = 0
-    while (archersList.size + secondArchList.size != 1) {
-        if (archersList.size != 0) {
-            for ((i, v) in archersList.withIndex()) {
-                if (check != 1) {
+
+    while (archersList.size != 1) {                                     // Check if there are more than one archer in our list
+        if (archersList[archersList.size - 1] == 0) {
+            archersList.removeAll { it == 0 }
+            for (i in 0 until archersList.size) {
+                if (i < archersList.size - 1) {
                     if (i % 2 == 0) {
-                        secondArchList.add(v)
-                        if (i < archersList.size - 1)
-                            archersList[i + 1] = 0
-                    }
-                }
-                else {
-                    if (i % 2 != 0) {
-                        secondArchList.add(v)
-                        archersList[i - 1] = 0
+                        if (archersList.size - 1 != i) archersList[i + 1] = 0
                     }
                 }
             }
-            if (archersList[archersList.size - 1] == 0) {
-                check = 0
-            } else {
-                check = 1
-            }
-            archersList.clear()
         }
         else {
-            for ((i, v) in secondArchList.withIndex()) {
-                if (check != 1) {
-                    if (i % 2 == 0) {
-                        archersList.add(v)
-                        if (i < secondArchList.size - 1)
-                            secondArchList[i + 1] = 0
-                    }
-                }
-                else {
-                    if (i % 2 != 0) {
-                        archersList.add(v)
-                        secondArchList[i - 1] = 0
-                    }
+            archersList.removeAll { it == 0 }
+            for (i in 0 until archersList.size) {
+                if (i % 2 != 0) {
+                    archersList[i - 1] = 0
                 }
             }
-            if (secondArchList[secondArchList.size - 1] == 0) {
-                check = 0
-            } else {
-                check = 1
-            }
-            secondArchList.clear()
+            if (archersList.size > 1)
+                if (archersList[archersList.size - 2] != 0)
+                    archersList[archersList.size - 1] = 0
         }
     }
-    print("Survivor Archer is ")
-    if (archersList.size == 1)
-    println(archersList.joinToString())
-    else
-    println(secondArchList.joinToString())
+
+    print("Survivor Archer is ${archersList.joinToString()}")
 }
